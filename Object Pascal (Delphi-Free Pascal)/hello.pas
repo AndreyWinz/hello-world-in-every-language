b3c1a6ece479f0e522b3c101856be9621d9fe393
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2025-11-25 19:56:49 +0400

    Create hello.pas

---
+++ Object Pascal (Delphi-Free Pascal)/hello.pas
@@ -0,0 +1,6 @@
+program Project1;
+{$APPTYPE CONSOLE} // Optional directive for console apps in Delphi GUI projects
+begin
+  writeln('Hello, World!');
+  // readln; // Optional: keeps the console window open until a key is pressed
+end.
