62720131b10f797f2eef04a4889d04cb83359900
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2025-11-25 19:45:42 +0400

    Create App.vue

---
+++ VueJS/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main id="app">
+    <h1 class="title">Hello, World!</h1>
+    <button @click="incrementCounter">Count: {{ counter }}</button>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  padding: 10px;
+}
+
+.title {
+  color: #5C6AC4;
+}
+</style>
