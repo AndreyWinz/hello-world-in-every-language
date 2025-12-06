bf9fa44c9bfb87f46d5c4922dbac47ab1dbb384f
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2025-12-06 16:46:40 +0400

    Create globals.h

---
+++ (SIASL)²/Interpreter/globals.h
@@ -0,0 +1,14 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+
+#include "ast.h"
+#include "cells.h"
+#include "stack.h"
+
+extern CELLMATRIX * environment;
+extern S_STACK * stack;
+extern unsigned char progempty;
+
+extern macrotable * table;
+#endif
