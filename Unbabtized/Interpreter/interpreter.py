db576950b7ee3e43c4e65824dcfb1dc083768ab6
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2025-12-06 19:52:45 +0400

    Create interpreter.py

---
+++ Unbabtized/Interpreter/interpreter.py
@@ -0,0 +1,24 @@
+import sys
+
+f = sys.argv[1]
+
+o = [0,[],[0]*1000,lambda x:o[4]("o[2]")+"x,o[2][x]%sy)" %x,
+     lambda x:x+".__setitem__(",open(f).read().split("."),0,
+     1,None,lambda x,y:o.__setitem__(x,y),lambda:o[9](7,o[6]
+     .find("°")),lambda:len(o[6][o[8]:o[8]+1]) and o[6][o[8]
+     :o[8]+1] in "0123456789",lambda:o[9](6,o[6][:o[7]]+"o["\
+     "2]["+o[6][o[7]+1:o[8]]+"]"+o[6][o[8]:]),lambda:(o[11](
+     )and(o[9](8,o[8]+1),o[13]())or 0),lambda:o[7]>=0 and(o[
+     9](8,o[7]+1),o[13](),o[12]()),lambda:"(lambda x=None,y"\
+     "=None:%s)(%s)"%({'!':o[4]("o[2]")+"x,y)",'@':"sys.std"\
+     "out.write(str(x)+chr(10))",':':"sys.stdout.write(chr("\
+     "x))",'~':o[3]("+"),'(':o[3]("-"),')':o[3]("*"),'[':o[3
+     ]("/"),"A":o[3]("<"),"§":o[3]("<="),"$":o[3]("=="),"%":
+     o[3](">"),"&":o[3](">="),"/":o[3]("<>"),',':"o[1].appe"\
+     "nd(o[0])",'-':"o[2][x] and ("+o[4]("o")+"0,o[1][0]-1)"\
+     ",o[1].__setslice__(1,len(o[1]),o[1][1:]))or 0",}[o[6][
+     0]],o[6][1:]),lambda:(o[9](6,o[5][o[0]]),o[10](),o[14](
+     ),eval(o[15]()),o[9](0,o[0]+1)),lambda:o[0] < len(o[5])
+     and(o[16](),o[17]())]
+
+o[17]()
