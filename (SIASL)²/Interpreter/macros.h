feb24752924b05064714a3f3dc6b016a3b8a29d9
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2025-12-06 16:48:02 +0400

    Create macros.h

---
+++ (SIASL)²/Interpreter/macros.h
@@ -0,0 +1,27 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#include "parser.tab.h"
+#include "ast.h"
+
+
+#define _TABLE_DEF_SIZE 12
+#define _ARRENT_DEF_SIZE 6
+
+extern macrotable * init_table( unsigned short size, unsigned arrsize);
+
+extern void free_entryarray ( entryarray * array);
+extern void free_table( macrotable * mtable);
+
+extern macroentry * mkMacroEntry ( Symbol sym, program * prog);
+extern entryarray * initArr( int size);
+
+extern void appArr( entryarray * arr, macroentry * element);
+extern unsigned hashSymbol( Symbol sym, unsigned size);
+
+extern void appTable ( macrotable * mtable, macroentry * entry);
+extern program * findProg( macrotable * mtable, Symbol sym);
+
+extern program * instructToProg( instruction * instruct);
+
+#endif
