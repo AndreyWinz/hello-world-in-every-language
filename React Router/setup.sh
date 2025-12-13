a96b6e0a56937dfc63fed8a84217e3a89a4e0959
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2025-12-13 14:21:42 +0400

    Create setup.sh

---
+++ React Router/setup.sh
@@ -0,0 +1,10 @@
+# Create a new Vite project
+npm create vite@latest react-router-example
+cd react-router-example
+
+# Follow the prompts to select "React" and "JavaScript/TypeScript"
+# Install project dependencies
+npm install
+
+# Install React Router
+npm install react-router
