21d231b12ec36de8682f87a33afecf4512f5223b
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2025-12-13 14:24:35 +0400

    Create hello.js

---
+++ Fastify/hello.js
@@ -0,0 +1,23 @@
+'use strict' // Use strict mode for better code quality
+
+const fastify = require('fastify')({
+  logger: true // Enable logging
+})
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' }) // Listen on port 3000
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
